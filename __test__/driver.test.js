aa6acafe8a27ae53042c2fd01188ec5b9724441c
Dina Ayoub <dinaayoub@hotmail.com>
2020-12-22 13:13:08 -0800

    modularized it with namespaces

--- __test__/driver.test.js
+++ __test__/driver.test.js
@@ -1,5 +1,5 @@
 'use strict';
-const driver = require('../src/driver');
+const driver = require('../src/driver/driver');
 const faker = require('faker');
 
 describe('DRIVER', () => {
---
+++ src/vendor/vendor.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('dotenv').config();
+var faker = require('faker');
+const io = require('socket.io-client');
+const port = process.env.config || 3333;
+const host = `http://localhost:${port}`;
+const vendorConnection = io.connect(`${host}/vendor-system`);
+
+vendorConnection.on('delivered', thankDriver);
+
+function thankDriver(payload) {
+  console.log('VENDOR: Thank you for delivering', payload.orderID);
+}
+
+function orderReady() {
+  var order = {
+    storeName: process.env.STORE_NAME,
+    storeID: process.env.STORE_ID,
+    orderID: faker.random.uuid(),
+    name: faker.name.findName(),
+    address: faker.address.streetAddress() + ', ' + faker.address.city() + ', ' + faker.address.stateAbbr() + ' ' + faker.address.zipCode(),
+  };
+  vendorConnection.emit('pickup-ready', order);
+}
+
+setInterval(orderReady, 5000);
+
+module.exports = { thankDriver, orderReady };
